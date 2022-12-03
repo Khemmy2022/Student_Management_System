a7524d082c68d435001edabc60908dab09ccc148
Khemmy Charles <olukemimcu@gmail.com>
2022-12-03 16:49:29 -0600

    I just saved my file

---
+++ MyStudent.sql
@@ -0,0 +1,15 @@
+Create database studentDB;
+USE studentDB;
+Create table studentTable(
+    id int not null auto_increment primary key,
+    name varchar(255) not null,
+    class varchar(255) not null,
+);
+insert into studentTable(name, class)
+values ("mike", "ss1"),
+("jude", "ss1")
+("john", "ss1")
+("chidi", "ss3")
+("ada", "jss2");
+
+select * from studentTable;
